32803c388aad75567ae49cfd01f1a2100746d64d
yahto <401306275@qq.com>
2017-05-19 15:44:32 +0800

    更改系统常量 分页最多25条最少2条

--- src/com/pipi/common/constant/SystemConstant.java
+++ src/com/pipi/common/constant/SystemConstant.java
@@ -21,16 +21,11 @@ public class SystemConstant {
 	public static final String TIME_ONLY = "HH:mm:ss";
 	
 	/** 分页单个页面默认显示条数 **/
-	public static final int PAGE_SIZE = 100;//Integer.MAX_VALUE;
+	public static final int PAGE_SIZE = 25;//Integer.MAX_VALUE;
 	
-	/** 系统默认收包裹截止时间 **/
-	public static final String DEADLINE = "2016-11-21 23:59:59";
-
 	/** 管理员登录名,系统默认的一个管理员帐号 */
-	public static final String USER_ADMIN_LOGINNAME = "adminSerivce";
-	
-	/**临时订单编号的自动递增时，保存当前的编号*/
-	public static final String TEMPORDER_NO = "/TempOrderNo.txt";
+	public static final String USER_ADMIN_LOGINNAME = "admin";
+
 	/** 分页单个页面默认显示条数 **/
 	public static final Double WEIGHT_LIMIT = 2.0;//Integer.MAX_VALUE;
 }
