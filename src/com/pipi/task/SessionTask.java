71653ba4fcea5650a4bdd3e2c881d1f6b6fa8118
颜彤 <yahto.yan@ximalaya.com>
2017-10-28 10:53:08 +0800

    ...

--- src/com/pipi/task/SessionTask.java
+++ src/com/pipi/task/SessionTask.java
@@ -1,8 +1,6 @@
 package com.pipi.task;
 
 import com.pipi.common.session.MySessionContext;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
